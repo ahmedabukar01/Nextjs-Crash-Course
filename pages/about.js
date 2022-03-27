0e7b3c8e1a6f2f29922e710133c159e4f899d164
ahmed <ahmedlearn01@gmail.com>
2022-03-27 13:49:54 -0700

    new page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import { Head } from "next/document"
+
+const about = () => {
+  return (
+    <div>
+        <Head>
+            <title>About Page</title>
+        </Head>
+        <h1>About Page....</h1>
+    </div>
+  )
+}
+
+export default about
